685c014b175bd6d772deb50a6493dd2bb65ff782
jokersus <33132401+jokersus@users.noreply.github.com>
2018-07-26 22:26:02 +0400

    Forward direct messages to owners

---
+++ events/bot/message.js
@@ -0,0 +1,15 @@
+const { RichEmbed } = require('discord.js');
+
+module.exports = message => {
+    if (!message.guild && !message.author.bot) {
+        message.client.owners.forEach(owner => {
+            if (message.author.id !== owner) {
+                owner.send(new RichEmbed()
+                    .setColor('BLUE')
+                    .setTitle('Bot has received a DM')
+                    .addField(message.author.tag, message.content)
+                );
+            }
+        });
+    }
+}
