e4db7a2f8bc8fe0578956902384f189b479067ab
jokersus <33132401+jokersus@users.noreply.github.com>
2018-08-06 22:58:31 +0400

    Goodbye message actually sends in a channel ow

--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -3,7 +3,8 @@ module.exports = (member, Client) => {
     let entry = Client.provider.get(member.guild, 'guildMemberRemove', false)  
     if (entry.message && entry.channel) {
         let message = entry.message;
-        message = JSON.parse(message.replace('$MENTION$', member.toString()).replace('$NAME$', member.user.tag).replace('$MEMBER_COUNT$', member.guild.memberCount).replace('$AVATAR$', member.user.avatarURL));
+        message = JSON.parse(message.replace('$MENTION$', member.toString()).replace('$NAME$', member.user.tag).replace('$MEMBER_COUNT$', member.guild.memberCount).replace('$AVATAR$', member.user.avatarURL))
+        member.guild.channels.find(val => val.id === entry.channel).send(message.content, new RichEmbed(message));
     }
     if (entry.log) {
         member.guild.channels.find(val => val.id === entry.log).send(new RichEmbed()
