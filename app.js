eb51d54148bce432d05643091f679b3ad7ff0d2f
jokersus <33132401+jokersus@users.noreply.github.com>
2018-08-15 02:25:10 +0400

    Remove unnecessary comments

--- app.js
+++ app.js
@@ -14,15 +14,6 @@ client.defaultColor = config.defaultColor;
 
 client.setProvider(
     sqlite.open(path.join(__dirname, 'settings.sqlite3')).then(db => {
-        // let knex = require('knex')({
-        //     dialect: 'sqlite3',
-        //     connection: {
-        //       filename: 'settings.sqlite3'
-        //     },
-        //     useNullAsDefault: true
-        //   });
-        // client.database = db;
-        // client.database.handler = knex;
         return new Commando.SQLiteProvider(db)
     })
 ).catch(console.error);
@@ -40,10 +31,8 @@ client.registry
     .registerDefaultCommands({
         ping: false
     })
-    .registerCommandsIn(path.join(__dirname, 'commands'))
+    .registerCommandsIn(path.join(__dirname, 'commands'));
 
-
-// client.on('message', message => {}); 
 
 client.login(process.env.DISCORD_TOKEN || config.token).catch(fail => {
     console.log('Failed to log in\n' + fail.toString());
