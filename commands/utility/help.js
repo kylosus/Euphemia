6a53638497c89325e50f520dceea7ea72d10afeb
jokersus <33132401+jokersus@users.noreply.github.com>
2018-08-15 02:28:11 +0400

    Better embed structure; cached embed for later use

--- commands/utility/help.js
+++ commands/utility/help.js
@@ -1,5 +1,7 @@
 const { Command } = require('discord.js-commando');
 const { RichEmbed } = require('discord.js');
+const packageJSON = require('../../package.json');
+let cache;
 
 module.exports = class extends Command {
     constructor(client) {
@@ -12,39 +14,55 @@ module.exports = class extends Command {
         });
     }
 
-   async run(message) {
-       let args = message.content.split(' ');
-       if (args.length === 1) {
-           return message.channel.send(new RichEmbed()
-                .setTitle(`${message.client.user.tag} commands`)
-                .setThumbnail(message.client.user.avatarURL || message.client.user.defaultAvatarURL)
-                .setColor(message.client.defaultColor)
-                .setDescription(`To run a command in ${message.guild ? message.guild.name : 'any server'},
-                use ${Command.usage('command', message.guild ? message.guild.commandPrefix : null, this.client.user)}.
-                For example, ${Command.usage('ping', message.guild ? message.guild.commandPrefix : null, this.client.user)}.`)
-                .addField('Available commands', message.client.registry.commands.map(command => `**${message.client.commandPrefix}${command.name}**`).join('\n'), false)
-            );
-       } else {
-           const result = message.client.registry.commands.get(args[1]);
-           if (!result) {
+    async run(message) {
+
+        let args = message.content.split(' ');
+        if (args.length === 1) {
+
+            if (cache) {
+                return message.channel.send(cache)
+            } else {
+
+                const embed = new RichEmbed()
+                    .setTitle(`${message.client.user.username} commands`)
+                    .setThumbnail(message.client.user.avatarURL || message.client.user.defaultAvatarURL)
+                    .setColor(message.client.defaultColor)
+                    .addBlankField()
+                    .setFooter(`♥ Made with love by ${packageJSON.author}`);
+
+                this.client.registry.groups.forEach(group => {
+                    embed.addField(group.name, group.commands.map(command => `**${command.name}**: ${command.description}`).join('\n'));
+                    embed.addBlankField();
+                });
+
+                cache = embed;
+                return message.channel.send(cache);
+            }
+
+        } else {
+
+            const result = message.client.registry.commands.get(args[1]);
+            if (!result) {
                 return message.channel.send(new RichEmbed()
                     .setColor('RED')
                     .setTitle('Command not found')
                 );
-           } else {
+            } else {
                 const embed = new RichEmbed()
-                embed.setTitle(`Command name: ${result.name}`);
-                embed.setThumbnail(message.client.user.avatarURL || message.client.user.defaultAvatarURL);
-                embed.setColor(message.client.defaultColor);
-                embed.setDescription(result.description);
+                    .setTitle(`Command name: ${result.name}`)
+                    .setThumbnail(message.client.user.avatarURL || message.client.user.defaultAvatarURL)
+                    .setColor(message.client.defaultColor)
+                    .setDescription(result.description);
+
                 if (result.aliases.length > 0) {
                     embed.addField('Aliases', result.aliases.join('\n'), false)
                 }
                 if (result.examples) {
                     embed.addField('Examples', '```' + result.examples.join('\n') + '```', false);
                 }
+
                 return message.channel.send(embed);
-           }
-       }
+            }
+        }
     }
 }
