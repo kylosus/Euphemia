2f07b05b387efe647e9e1db32391c7590ae0850e
jokersus <33132401+jokersus@users.noreply.github.com>
2018-08-17 02:12:08 +0400

    Say command utilizing EuphemiaEmbed,js

---
+++ commands/owner/say.js
@@ -0,0 +1,34 @@
+const { Command } = require('discord.js-commando');
+const { RichEmbed } = require('discord.js');
+const EuphemiaEmbed = require('../../util/EuphemiaEmbed.js');
+
+module.exports = class extends Command {
+    constructor(client) {
+        super(client, {
+            name: 'say',
+            group: 'owner',
+            memberName: 'say',
+            description: 'Says something',
+            details: 'Says something. Supports embeds',
+            userPermissions: ['BAN_MEMBERS'],
+            examples: [`${client.commandPrefix}say something`, `${client.commandPrefix}say {JSON}`],
+            ownerOnly: true
+        });
+    };
+
+    async run(message) {
+        let args = message.content.split(' ');
+        if (args.length === 1) {
+            return;
+        } else {
+            if (args[1].startsWith('{')) {
+                const json = args.splice(1).join(' ');
+                if (EuphemiaEmbed.validate(json)) {
+                    message.channel.send(EuphemiaEmbed.build(json));
+                }
+            } else {
+                return message.channel.send(args.splice(1).join(' '));
+            }
+        }
+    }
+}
