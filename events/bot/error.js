dfd8587173d9b5901ac40e8c3364230d150521ad
jokersus <33132401+jokersus@users.noreply.github.com>
2018-08-20 22:17:27 +0400

    Remove direct message notification to owners

--- events/bot/error.js
+++ events/bot/error.js
@@ -1,12 +1,3 @@
-const { RichEmbed } = require('discord.js');
-
-module.exports = (error, client) => {
-    console.log(error);
-    client.owners.forEach(owner => {
-        owner.send(new RichEmbed()
-            .setColor('RED')
-            .setTitle('An error has occured')
-            .setDescription('Check console for details')
-        );
-    })
+module.exports = error => {
+    console.log(JSOn.stringify(error));
 }
--- events/bot/ready.js
+++ events/bot/ready.js
@@ -1,25 +1,3 @@
-const { RichEmbed } = require('discord.js');
-
 module.exports = client => {
   console.log(`Logged in as ${client.user.tag}!`);
-
-  let oldLog = console.log;
-  if (!console.hijacked) {
-	  console.hijacked = true;
-	  let oldLog = console.log;
-      console.log = message => {
-		  client.owners.forEach(owner => {
-			  oldLog.apply(console, [message]);
-			  owner.send(new RichEmbed()
-					.setColor([233, 91, 169])
-					.setTitle('new console output')
-				).then(() => {
-					if (message > 1994) {
-						message = message.substring(0, 1994);
-					}
-					owner.send('```' + message + '```');
-				})
-		  });
-		}
-    }
 }
