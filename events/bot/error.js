9285d60f610fec672adb96c47e18581365c1101f
jokersus <33132401+jokersus@users.noreply.github.com>
2018-08-21 16:12:49 +0400

    Actually remove console log hijacking

--- events/bot/error.js
+++ events/bot/error.js
@@ -1,12 +1,1 @@
-const { RichEmbed } = require('discord.js');
-
-module.exports = (error, client) => {
-    console.log(error);
-    client.owners.forEach(owner => {
-        owner.send(new RichEmbed()
-            .setColor('RED')
-            .setTitle('An error has occured')
-            .setDescription('Check console for details')
-        );
-    })
-}
+module.exports = console.error;
