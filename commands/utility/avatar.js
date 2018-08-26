8688afe00b40f9f70111a20516397b7c2b29a041
jokersus <33132401+jokersus@users.noreply.github.com>
2018-08-26 21:40:44 +0400

    Bug fixes

--- commands/utility/avatar.js
+++ commands/utility/avatar.js
@@ -7,25 +7,20 @@ module.exports = class extends Command {
             name: 'avatar',
             group: 'utility',
             memberName: 'avatar',
-            description: 'Shows avatar of a given user',
-            aliases: ['disc'],
-            throttling: {
-                usages: 1,
-                duration: 15
-            }
+            description: 'Shows avatar of a given user'
         });
     }
 
    async run(message) {
        if (message.guild) {
-           const member = message.guild.members.get(message.content.match(/\d{18}/)[0]) ||
-                    message.member;
+           const match = message.content.match(/\d{18}/);
+           const member = match ? message.guild.members.get(match[0]) : message.member;
            return message.channel.send(new RichEmbed()
                 .setDescription(`${member.toString()}'s avatar`)
                 .setColor(member.displayColor)
                 .setImage(member.user.avatarURL ?
-                    `https://cdn.discordapp.com/avatars/${member.id}/${member.user.avatar}?size=1024` :
-                        member.user.defaultAvatarURL
+                    `https://cdn.discordapp.com/avatars/${member.id}/${member.user.avatar}${member.user.avatar.startsWith('a') ?
+                        '.gif' : ''}?size=1024` : member.user.defaultAvatarURL
                 )
             );
        } else {
@@ -33,8 +28,8 @@ module.exports = class extends Command {
                .setDescription('Your avatar')
                .setColor(global.BOT_DEFAULT_COLOR)
                .setImage(message.author.avatarURL ?
-                   `https://cdn.discordapp.com/avatars/${message.author.id}/${message.authpr.avatar}?size=1024` :
-                        message.author.defaultAvatarURL
+                   `https://cdn.discordapp.com/avatars/${message.author.id}/${message.author.avatar}${message.author.avatar.startsWith('a') ?
+                       '.gif' : ''}?size=1024` : message.author.defaultAvatarURL
                 )
            );
        }
