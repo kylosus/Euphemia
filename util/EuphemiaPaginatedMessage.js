153ccd2c371f6b414af1cac5d5376f186e4e90e5
jokersus <33132401+jokersus@users.noreply.github.com>
2018-08-29 02:09:19 +0400

    No need to react if there is only one page

--- util/EuphemiaPaginatedMessage.js
+++ util/EuphemiaPaginatedMessage.js
@@ -1,12 +1,13 @@
 const EuphemiaLinkedList = require('./EuphemiaLinkedList.js');
 
 module.exports = (embeds, message) => {
-    
+
     message.channel.send(embeds[0].setFooter(`1/${embeds.length}`)).then(async botMessage => {
         const embedList = new EuphemiaLinkedList(embeds);
-        await botMessage.react('⬅');
-        await botMessage.react('➡');
-
+        if (embeds.length > 1) {
+            await botMessage.react('⬅');
+            await botMessage.react('➡');
+        }
         const listener = ((messageReaction, reactionUser, ownMessage = botMessage, callerUserId = message.author.id, list = embedList) => {
             if (reactionUser.id === callerUserId) {
                 messageReaction.remove(reactionUser);
@@ -22,7 +23,7 @@ module.exports = (embeds, message) => {
             }
         });
         message.client.on('messageReactionAdd', listener);
-        
+
         message.client.setTimeout((message, listener) => {
             message.clearReactions();
             message.client.removeListener('messageReactionAdd', listener);
