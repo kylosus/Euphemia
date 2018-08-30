caa7a04084940be0239db65c2e08b98da2ea442b
jokersus <33132401+jokersus@users.noreply.github.com>
2018-08-30 14:09:36 +0400

    Change port

--- dbl.js
+++ dbl.js
@@ -1,7 +1,7 @@
 const DBL = require('dblapi.js');
 
 module.exports = (token, password, client) => {
-    const dbl = new DBL(token, { webhookPort: 5000, webhookAuth: password }, client);
+    const dbl = new DBL(token, { webhookPort: 3001, webhookAuth: password }, client);
     dbl.webhook.on('vote', console.log);
     dbl.on('posted', () => {
         console.log('Server count posted!');
