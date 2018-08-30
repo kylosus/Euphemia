eaaf017ebf3451f3efa31ce964514fb03d7d1b55
jokersus <33132401+jokersus@users.noreply.github.com>
2018-08-30 13:55:32 +0400

    Remove unnecessary console logging

--- dbl.js
+++ dbl.js
@@ -2,8 +2,6 @@ const DBL = require('dblapi.js');
 
 module.exports = (token, password, client) => {
     const dbl = new DBL(token, { webhookPort: 5000, webhookAuth: password }, client);
-    console.log(dbl);
-    dbl.webhook.on('ready', console.log);
     dbl.webhook.on('vote', console.log);
     dbl.on('posted', () => {
         console.log('Server count posted!');
