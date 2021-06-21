a555322706948ddae6a63d8e5be5578cf8f22b4b
jokersus <lolivampireslave@gmail.com>
2021-06-21 20:31:13 +0400

    Upgraded urban to new framework. Upgraded to new ud library

--- commands/utility/urban.js
+++ commands/utility/urban.js
@@ -1,46 +1,50 @@
-const { Command }				= require('discord.js-commando');
-const { RichEmbed }				= require('discord.js');
 const EuphemiaPaginatedMessage	= require('../../util/EuphemiaPaginatedMessage.js');
 const ud						= require('urban-dictionary');
 const udIcon					= 'https://cdn.discordapp.com/attachments/352865308203024395/479997284117905440/ud.png';
 
-module.exports = class extends Command {
+const { MessageEmbed } = require('discord.js');
+
+const ECommand = require('../../lib/ECommand');
+const ArgConsts = require('../../lib/Argument/ArgumentTypeConstants');
+
+module.exports = class extends ECommand {
 	constructor(client) {
 		super(client, {
-			name: 'urban',
-			group: 'utility',
-			memberName: 'urban',
-			description: 'Returns Urban Dictionary definitions',
-			aliases: ['ud']
+			aliases: ['urban', 'ud'],
+			description: {
+				content: 'Looks up Urban Dictionary definitions',
+				usage: '[prompt]',
+				examples: ['ud malding']
+			},
+			args: [
+				{
+					id: 'text',
+					type: ArgConsts.TEXT,
+					message: 'Please provide text to look up.'
+				}
+			],
+			guildOnly: false,
+			nsfw: false,
+			ownerOnly: false
 		});
 	}
 
-	async run(message) {
-		const args = message.content.split(' ').slice(1);
-		if (!args.length) {
-			return message.channel.send(new RichEmbed()
-				.setTitle('Please enter search terms')
-				.setColor('RED')
-			);
-		} else {
-			const query = args.join(' ');
-			const result = await ud.term(query);
-
-			if (!result) {
-				return message.channel.send(new RichEmbed()
-					.setColor('RED')
-					.setTitle(`Could not find definitions for ${query}`)
-				);
-			}
-
-			EuphemiaPaginatedMessage(result.entries
-				.map(entry =>
-					new RichEmbed()
-						.setColor('GREEN')
-						.setAuthor(entry.word, udIcon)
-						.setDescription(entry.definition)
-				), message
-			);
+	async run(message, args) {
+		try {
+			return await ud.define(args.text);
+		} catch (err) {
+			throw `Could not find any definitions for ${args.text}`;
 		}
+	}
+
+	async ship(message, result) {
+		return EuphemiaPaginatedMessage(result
+			.map(e =>
+				new MessageEmbed()
+					.setColor('GREEN')
+					.setAuthor(e.word, udIcon)
+					.setDescription(e.definition)
+			), message
+		);
 	}
 };
