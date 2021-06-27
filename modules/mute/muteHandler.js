b462fe040aed0b2d96aea711e8b02ad6c0063297
jokersus <lolivampireslave@gmail.com>
2021-06-27 18:20:58 +0400

    Removing for now. Will fix up the definitions later

--- modules/mute/muteHandler.js
+++ modules/mute/muteHandler.js
@@ -1,11 +1,6 @@
 const db = require('./db');
 
 const muteMember = async (guild, member, mutedRole, reason, duration) => {
-	// if (member.roles.cache.has(mutedRole.id)) {
-	// 	throw 'Member is already muted';
-	// }
-
-	// Will fix this
 	await db.insert(guild.id, member.id, mutedRole.id, reason, duration.toISOString());
 };
 
