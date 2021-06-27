ce8311372d5855efb5b987d2d605afaa5adda120
jokersus <lolivampireslave@gmail.com>
2021-06-27 18:21:13 +0400

    Added mute watcher to periodically check the db and unmute expired entries

---
+++ modules/mute/watcher.js
@@ -0,0 +1,39 @@
+const db = require('./db');
+
+const INTERVAL = 2000;
+
+const init = async client => {
+	client.setInterval(async () => {
+		const result = await db.getExpired();
+
+		await Promise.all(result.map(async r => {
+			db.remove(r.guild, r.member).then();
+
+			const guild = client.guilds.cache.get(r.guild);
+
+			if (!guild) {
+				return;
+			}
+
+			const member = await guild.members.fetch(r.member);
+
+			if (!member) {
+				return;
+			}
+
+			const mutedRole = member.roles.cache.get(r.mutedRole);
+
+			if (!mutedRole) {
+				return;
+			}
+
+			await member.roles.remove(mutedRole, 'Mute expired');
+
+			client.emit('muteExpired', member);
+		}));
+	}, INTERVAL);
+};
+
+module.exports = {
+	init
+};
