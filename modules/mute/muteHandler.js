5520becb666fe90ed5bd04abb2b67e218045e1fe
jokersus <lolivampireslave@gmail.com>
2021-06-27 18:21:06 +0400

    Added functions for unmuting

--- modules/mute/muteHandler.js
+++ modules/mute/muteHandler.js
@@ -4,6 +4,17 @@ const muteMember = async (guild, member, mutedRole, reason, duration) => {
 	await db.insert(guild.id, member.id, mutedRole.id, reason, duration.toISOString());
 };
 
+const unmuteMemberRaw = async (guild, member, mutedRole) => {
+	// ~
+};
+
+const unmuteMember = async (guild, member, mutedRole, reason = 'Unmuted') => {
+	await member.roles.remove(mutedRole, reason);
+	await db.remove(guild.id, member.id);
+};
+
 module.exports = {
-	muteMember
+	muteMember,
+	unmuteMemberRaw,
+	unmuteMember
 };
