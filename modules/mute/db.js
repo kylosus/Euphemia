3899928625fb73b4c68be3d4480466e836f6c591
jokersus <lolivampireslave@gmail.com>
2021-06-27 18:18:52 +0400

    Added getExpired statement to get all expired mutes

--- modules/mute/db.js
+++ modules/mute/db.js
@@ -16,13 +16,24 @@ const init = async (client, db) => {
 	`);
 
 	STATEMENTS.insert = await db.prepare(`INSERT OR REPLACE INTO ${TABLE_NAME} VALUES (?, ?, ?, ?, ?)`);
+	STATEMENTS.getExpired = await db.prepare(`
+		SELECT
+			CAST(guild as TEXT) as guild,
+			CAST(member as TEXT) as member,
+			CAST(mutedRole as TEXT) as mutedRole
+		FROM ${TABLE_NAME} where expires < date('now')
+	`);
 };
 
 const insert = async (guild, member, mutedRole, reason, expires) => {
 	await STATEMENTS.insert.run(guild, member, mutedRole, reason, expires);
 };
 
+const getExpired = async () => {
+	return await STATEMENTS.getExpired.all();
+};
 module.exports = {
 	init,
-	insert
+	insert,
+	getExpired,
 };
