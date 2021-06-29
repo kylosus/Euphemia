cbba2f58650de078eaaf27faa471c51be9fa88d6
jokersus <lolivampireslave@gmail.com>
2021-06-29 17:17:32 +0400

    Changed second argument to duration to be compatible with database watcher modules

--- events/loggable/guildMemberMuted.js
+++ events/loggable/guildMemberMuted.js
@@ -1,13 +1,15 @@
 const { MessageEmbed } = require('discord.js');
 
-module.exports = (member, duration, moderator) => {
-	const entry = member.client.provider.get(member.guild, 'log', {guildMemberMuted: null});
+const moment = require('moment');
 
-	if (!entry.guildMemberMuted) {
+module.exports = (member, expires, moderator) => {
+	const {guildMemberMuted: channelID} = member.client.provider.get(member.guild, 'log', {guildMemberMuted: null});
+
+	if (!channelID) {
 		return;
 	}
 
-	const channel = member.guild.channels.resolve(entry.guildMemberMuted);
+	const channel = member.guild.channels.cache.get(channelID);
 
 	if (!channel) {
 		return;
@@ -18,7 +20,7 @@ module.exports = (member, duration, moderator) => {
 		.setTitle('🔇 User muted')
 		.setThumbnail(member.user.avatarURL)
 		.addField('User', `${member.toString()} \`${member.id}\``, false)
-		.addField('Duration', `${duration ? duration : '-'} minutes`)
+		.addField('Expires', `${expires ? moment(expires).fromNow() : 'Never'}`)
 		.addField('Moderator', moderator.toString())
 	);
 };
