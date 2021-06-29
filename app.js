f251b12caf6a85dff722b8366ab551a975255e0f
jokersus <lolivampireslave@gmail.com>
2021-06-29 17:14:55 +0400

    Cleaned up app.js; Re-enabled dotenv and config files; added intents (untested)

--- app.js
+++ app.js
@@ -1,94 +1,53 @@
 require('dotenv').config();
 
-const EClient = require('./lib/EClient');
-const ECommandHandler = require('./lib/ECommandHandler');
+const path				= require('path');
+const sqlite3			= require('sqlite3');
+const sqlite			= require('sqlite');
 
-const config = require('./config.json');
-const path = require('path');
-// const sqlite		= require('sqlite');
+const { Intents }		= require('discord.js');
 
-// const client = new Commando.Client({
-// 	owner: process.env.BOT_OWNER || config.owner,
-// 	commandPrefix: process.env.BOT_PREFIX || config.prefix || ';',
-// 	disableEveryone: true,
-// 	unknownCommandResponse: false,
-// 	disabledEvents: [
-// 		'GUILD_SYNC',
-// 		'GUILD_DELETE',
-// 		'CHANNEL_CREATE',
-// 		'CHANNEL_DELETE',
-// 		'CHANNEL_UPDATE',
-// 		'CHANNEL_PINS_UPDATE',
-// 		'MESSAGE_DELETE_BULK',
-// 		'USER_NOTE_UPDATE',
-// 		'USER_SETTINGS_UPDATE',
-// 		'PRESENCE_UPDATE',
-// 		'VOICE_STATE_UPDATE',
-// 		'TYPING_START'
-// 	]
-// });
+const EClient			= require('./lib/EClient');
+const ECommandHandler	= require('./lib/ECommandHandler');
+const SQLiteProvider	= require('./lib/Provider/SQLiteProvider');
 
-/*
+const config			= require('./config.json');
+const modules			= require('./modules');
 
-const ArgumentType = require('./lib/Argument/ArgumentType');
-// const RegexParser = require('./lib/Argument/RegexParser');
-const ArgumentParser = require('./lib/Argument/ArgumentParser');
-const ArgConstants = require('./lib/Argument/ArgumentTypeConstants');
-
-
-// const parser = new RegexParser([/(?<=<@)(\d+)(?=>)/g, /\d/]);
-// const parser = new RegexParser([/<@\d+>/g, /\d+/g, /aduh121231/g]);
-const argParser = new ArgumentParser([
-	{
-		label: 'test1',
-		// type: new ArgumentType(/<@\d+>/g),
-		type: ArgConstants.MEMBERS,
-		message: 'test1 message'
-	},
-	{
-		label: 'test2',
-		type: new ArgumentType(/\d+/g),
-		message: 'test2 message'
-	}
-]);
-
-try {
-	const parsed = argParser.parse('<@22222> asdiajsd <@1231> 5');
-	console.log(parsed);
-} catch (err) {
-	console.log(`Error parsing: ${err}`);
-}
-
-
-// const a = parser.parse('<@22222> asdiajsd <@1231> 5');
-// console.log(a);
-return;
-
- */
-
-
-const { join } = require('path');
-
-const sqlite3 = require('sqlite3');
-const sqlite = require('sqlite');
-
-const SQLiteProvider = require('./lib/Provider/SQLiteProvider');
-
-// const commandsPath = join(__dirname, '..', 'commands/');
-// const listenersPath = join(__dirname, '..', 'listeners/');
-
-const modules = require('./modules');
-
-// enable partials
 class Client extends EClient {
 	constructor() {
 		super(
 			{
-				ownerIDs: ['275331662865367040'],
-				defaultColor: [233, 91, 169]
+				ownerIDs: process.env.BOT_OWNER?.split(',') ?? config.owners,
+				defaultColor: config.defaultColor || [233, 91, 169]
 			},
 			{
-				disableEveryone: true
+				disableMentions: 'all',
+				// untested, might break
+				partials: [
+					'MESSAGE',
+					'GUILD_MEMBER',
+					'MESSAGE'
+				],
+				ws: {
+					intents: [
+						Intents.FLAGS.GUILDS,
+						Intents.FLAGS.GUILD_MEMBERS,
+						Intents.FLAGS.GUILD_BANS,
+						Intents.FLAGS.GUILD_BANS,
+						Intents.FLAGS.GUILD_EMOJIS,
+						// Intents.FLAGS.GUILD_INTEGRATIONS,
+						// Intents.FLAGS.GUILD_WEBHOOKS,
+						// Intents.FLAGS.GUILD_INVITES,
+						// Intents.FLAGS.GUILD_VOICE_STATES,
+						// Intents.FLAGS.GUILD_PRESENCES,
+						Intents.FLAGS.GUILD_MESSAGES,
+						Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+						// Intents.FLAGS.GUILD_MESSAGE_TYPING,
+						Intents.FLAGS.DIRECT_MESSAGES,
+						Intents.FLAGS.DIRECT_MESSAGE_REACTIONS,
+						// Intents.FLAGS.DIRECT_MESSAGE_TYPING
+					]
+				}
 			}
 		);
 
@@ -101,84 +60,16 @@ class Client extends EClient {
 				await modules.init(this, db);
 				return new SQLiteProvider(db);
 			})
-		);
+		).catch(console.error);
 
 		this.commandHandler = new ECommandHandler(this, {
-			prefix: ';',
-			path: join(__dirname, 'commands'),
-			// argumentDefaults: {
-			// 	otherwise: 'test',
-			// 	// prompt: {
-			// 	// 	start: 'hi',
-			// 	// 	timeout: 'Time ran out, command has been cancelled.',
-			// 	// 	ended: 'Too many retries, command has been cancelled.',
-			// 	// 	cancel: 'Command has been cancelled.',
-			// 	// 	retries: 0,
-			// 	// 	time: 0
-			// 	// }
-			// }
+			prefix: process.env.BOT_PREFIX || config.prefix || ';',
+			path: path.join(__dirname, 'commands'),
 		});
 	}
 }
 
 const client = new Client();
-
 require('./events/event.js')(client);
 
-// client.setInterval(() => {
-// 	console.log(client.guilds.cache.get('344556222755766283').settings);
-// }, 2000);
-
-// eslint-disable-next-line no-unused-vars
-client.login(process.env.BOT_TOKEN).then(_ => console.log('Logged in'));
-
-// console.log(client);
-
-//
-// client2.login(process.env.BOT_TOKEN);
-//
-// require('./events/event.js')(client);
-// global.BOT_DEFAULT_COLOR = config.defaultColor || [233, 91, 169];
-//
-// client.setProvider(
-// 	sqlite.open(path.join(__dirname, 'settings.sqlite3')).then(db => {
-// 		return new Commando.SQLiteProvider(db);
-// 	})
-// ).catch(console.error);
-//
-// client.registry
-// 	.registerDefaultTypes()
-// 	.registerGroups([
-// 		['anime', 'Anime and Manga commands'],
-// 		['bot', 'Public bot commands'],
-// 		['fun', 'Fun commands'],
-// 		['moderation', 'Moderation commands'],
-// 		['owner', 'Owner only commands'],
-// 		['setup', 'Server utility setup commands'],
-// 		['subscription', 'Tag subscription commands'],
-// 		['utility', 'Utility commands']
-// 	])
-// 	.registerDefaultGroups()
-// 	.registerDefaultCommands({
-// 		ping: false,
-// 		help: false
-// 	})
-// 	.registerCommandsIn(path.join(__dirname, 'commands'));
-//
-// client.messageStats = { received: 0, sent: 0, commands: 0 };	// Possible data race, but it's good enough
-//
-// // client.login(process.env.BOT_TOKEN || config.token).catch(error => {
-// // 	throw `Failed logging in\n${error.message}`;
-// // });
-//
-// client.on('commandBlock', (m, reason, data) => {
-// 	console.log('a');
-// });
-//
-// client.on('commandError', (command, error, message, args, fromPatter, result) => {
-// 	console.log('a');
-// });
-//
-// client.on('commandCancel', (command, reason, message, result) => {
-// 	console.log('a');
-// });
+client.login(process.env.BOT_TOKEN).catch(console.error);
