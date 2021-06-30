f1eceb3cd8b6ff7734ee8e0c46f63bc6b2bf2a76
jokersus <lolivampireslave@gmail.com>
2021-06-30 21:22:33 +0400

    Enabling paginatedmessage module

--- modules/index.js
+++ modules/index.js
@@ -1,13 +1,16 @@
 const mute = require('./mute');
 const moderation = require('./moderation');
+const paginatedmessage = require('./paginatedmessage');
 
-const init = async (client, db) => {
-	await mute.init(client, db);
-	await moderation.init(client, db);
+const init = async (client, _db) => {
+	await mute.init(client, _db);
+	await moderation.init(client, _db);
+	await paginatedmessage.init(client);
 };
 
 module.exports = {
 	init,
 	mute,
 	moderation,
+	PaginatedMessage: paginatedmessage.PaginatedMessage
 };
