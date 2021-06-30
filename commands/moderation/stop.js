8b159709a4cea2f80c8d9839bd5a6fbbda4f78ad
jokersus <lolivampireslave@gmail.com>
2021-06-30 19:57:44 +0400

    Removed commented out old code

--- commands/moderation/stop.js
+++ commands/moderation/stop.js
@@ -59,29 +59,6 @@ module.exports = class extends ModerationCommand {
 		}));
 		
 		return result;
-
-		// if (arg.toLowerCase().startsWith('off')) {
-		// 	await message.channel.overwritePermissions(everyone, { 'SEND_MESSAGES': true });
-		//
-		// 	return message.channel.send(new RichEmbed()
-		// 		.setColor('GREEN')
-		// 		.setTitle('Channel unlocked')
-		// 	);
-		// }
-		//
-		// const channel = await message.channel.overwritePermissions(everyone, { 'SEND_MESSAGES': false }, 'Euphemia stop command');
-		//
-		// // TODO: TRY-CATCH THIS
-		// await channel.overwritePermissions(
-		// 	message.member.roles
-		// 		.filter(role => role.hasPermission('MANAGE_GUILD'))
-		// 		.first() || message.author, { 'SEND_MESSAGES': true }, 'Euphemia stop command'
-		// );
-		//
-		// return channel.send(new RichEmbed()
-		// 	.setColor('RED')
-		// 	.setTitle('Channel locked down')
-		// );
 	}
 
 	async ship(message, result) {
