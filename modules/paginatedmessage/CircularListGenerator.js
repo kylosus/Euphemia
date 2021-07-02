05682aedf7478e10c4d8e1eda56e61a5c0bb03ce
jokersus <lolivampireslave@gmail.com>
2021-07-03 00:53:24 +0400

    Added new CircularList extension to generate with provided functions

---
+++ modules/paginatedmessage/CircularListGenerator.js
@@ -0,0 +1,45 @@
+const CircularList = require('./CircularList');
+
+module.exports = class extends CircularList {
+	constructor(array, length, nextGen, previousGen) {
+		super(array);
+		this._first = nextGen();
+		this._current = this._first;
+		this._currentIndex = 0;
+		this._length = length;
+		this._end = this._length - 1;
+		this._next = nextGen;
+		this._previous = previousGen;
+	}
+
+	next() {
+		if (this._currentIndex < this._end) {
+			this._current = this._next();
+			this._currentIndex++;
+			return this._current;
+		}
+
+		this._currentIndex = 0;
+		return this._first;
+	}
+
+	previous() {
+		if (this._currentIndex === 0) {
+			return this._current;
+		}
+
+		return this._previous();
+	}
+
+	get current() {
+		return this._current;
+	}
+
+	get currentIndex() {
+		return this._currentIndex;
+	}
+
+	get length() {
+		return this._length;
+	}
+};
