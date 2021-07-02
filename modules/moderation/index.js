00918293b65adbe4cc8a42731973a9f0965380c5
jokersus <lolivampireslave@gmail.com>
2021-07-02 20:01:50 +0400

    Registering module commands with new registerCommands function of ECommandHandler

--- modules/moderation/index.js
+++ modules/moderation/index.js
@@ -1,7 +1,9 @@
 const db = require('./db');
+const {join} = require('path');
 
 const init = async (client, _db) => {
 	await db.init(client, _db);
+	await client.commandHandler.registerCommands(join(__dirname, './commands'), 'Modlog');
 };
 
 module.exports = {
