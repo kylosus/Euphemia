b916a1290faab921e2df314b54e4fc3830db1308
jokersus <lolivampireslave@gmail.com>
2021-07-03 00:52:33 +0400

    Added new statements to fetch mod action logs

--- modules/moderation/db.js
+++ modules/moderation/db.js
@@ -25,6 +25,29 @@ const init = async (client, db) => {
 		    ${TABLE_NAME} (id, guild, action, moderator, target, aux, reason, passed, failedReason, timestamp)
 		VALUES ((SELECT IFNULL(MAX(id), 0) + 1 FROM ${TABLE_NAME} where guild = ?), ?, ?, ?, ?, ?, ?, ?, ?, ?)
 	`);
+
+	// guild, moderator, lastValue, limit
+	STATEMENTS.getModeratorPage = await db.prepare(`
+		SELECT
+			id, action, aux, passed,
+        	CAST(moderator as TEXT) as moderator,
+        	CAST(target as TEXT) as target
+		FROM ${TABLE_NAME}
+		WHERE guild = ? AND moderator = ?
+			AND id < ?
+		ORDER BY id DESC
+		LIMIT ?
+	`);
+
+	STATEMENTS.getTargetPage = await db.prepare(`
+		SELECT * FROM ${TABLE_NAME} where guild = ? and target = ?
+	`);
+
+	STATEMENTS.getModeratorTargetPage = await db.prepare(`
+		SELECT * FROM ${TABLE_NAME} where guild = ? and moderator = ? and target = ?
+	`);
+
+	STATEMENTS.getIdMax = await db.prepare(`SELECT MAX(id) as length from ${TABLE_NAME} where guild = ? LIMIT 1`);
 };
 
 const insert = async ({guild, action, moderator, target, aux, reason, passed, failedReason}) => {
@@ -40,8 +63,31 @@ const bulkInsert = async (params = []) => {
 	}));
 };
 
+// I am so sorry
+const getModeratorTargetPage = async ({guild, moderator, target, lastId = Number.MAX_SAFE_INTEGER, perPage = 5}) => {
+	return await STATEMENTS.getModeratorPage.all(guild, moderator, lastId, perPage);
+
+	/*
+	if (moderator && target) {
+		return await STATEMENTS.getModeratorTargetPage.all(guild, moderator, target);
+	}
+
+	if (moderator) {
+		return await STATEMENTS.getModeratorPage.all(guild, moderator);
+	}
+
+	return await STATEMENTS.getTargetPage.all(guild, target);
+	 */
+};
+
+const getIdMax = async guild => {
+	return await STATEMENTS.getIdMax.get(guild);
+};
+
 module.exports = {
 	init,
 	insert,
-	bulkInsert
+	bulkInsert,
+	getModeratorTargetPage,
+	getIdMax
 };
