774b6619d136d5a475183d8a3dd31d95d09fa5b0
jokersus <lolivampireslave@gmail.com>
2021-07-06 00:51:39 +0400

    Added modAction loggable event

---
+++ events/loggable/modAction.js
@@ -0,0 +1,46 @@
+const {MessageEmbed}	= require('discord.js');
+const moment			= require('moment');
+
+const COLOR = '#2CDDD7';
+
+module.exports = (guild, moderator, result) => {
+	const entry = guild.client.provider.get(guild, 'log', {modAction: null});
+
+	if (!entry.modAction) {
+		return;
+	}
+
+	const channel = guild.channels.cache.get(entry.modAction);
+
+	if (!channel) {
+		return;
+	}
+
+	const embed = new MessageEmbed()
+		.setColor(COLOR);
+
+	embed.setAuthor(`${moderator.user.tag} (${result.moderator})}`, moderator.user.displayAvatarURL());
+
+	const prefix = result.passed ? '✅' : '❌';	// Fix those later
+	embed.setDescription(`${prefix} Action ${result.action} -> <@${result.target}>`);
+	embed.addField('Reason', '```' + (result.reason || 'No reason provided') + '```');
+
+	if (!result.passed) {
+		embed.addField('Failed', '```' + (result.failedReason || 'Unknown reason') + '```');
+	}
+
+	if (result.action === 'MUTE') {
+		embed.addField('Muted for', (time => {
+			if (!time) {
+				return 'Forever';
+			}
+
+			const diff = moment.duration(moment(result.aux).diff(result.timestamp));
+			return `${diff.days()} days, ${diff.hours()} hours, ${diff.minutes()} minutes`;
+		})(result.aux));
+	}
+
+	embed.setTimestamp(result.timestamp);
+
+	return channel.send(embed);
+};
