3ba8089cdc68a6ba2649e5b8f22c62fc246a72e9
jokersus <lolivampireslave@gmail.com>
2021-07-05 21:03:56 +0400

    Added warn Moderation Command to add warnings to members

---
+++ commands/moderation/warn.js
@@ -0,0 +1,40 @@
+const {Permissions} = require('discord.js');
+
+const {ArgConsts} = require('../../lib');
+const {ModerationCommand, ModerationCommandResult} = require('../../modules/moderation');
+
+module.exports = class extends ModerationCommand {
+	constructor(client) {
+		super(client, {
+			actionName: 'warn',
+			aliases: ['warn'],
+			description: {
+				content:	'Warns a member.',
+				usage:		'<member> [member2...] <reason>',
+				examples:	['warn @member Some reason', 'warn @member1 @member2 Some other reason']
+			},
+			userPermissions: [Permissions.FLAGS.MANAGE_GUILD],
+			args: [
+				{
+					id:			'members',
+					type:		ArgConsts.MEMBERS,
+					message:	'Please mention members to warn'
+				},
+				{
+					id:			'reason',
+					type:		ArgConsts.REASON,
+					message:	'Please add a reason'
+				}
+			],
+			guildOnly: true,
+			ownerOnly: false,
+		});
+	}
+
+	async run(message, {members, reason}) {
+		const result = new ModerationCommandResult(reason);
+		members.forEach(m => result.addPassed(m));
+
+		return result;
+	}
+};
