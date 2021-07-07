f90f7346e47a69bae56010971c3ff336b5224e4b
jokersus <lolivampireslave@gmail.com>
2021-07-08 01:16:03 +0400

    Added guild-specific command enabling/disabling

---
+++ commands/server/disable.js
@@ -0,0 +1,45 @@
+const {Permissions}			= require('discord.js');
+const {ArgConsts, ECommand}	= require('../../lib');
+
+module.exports = class extends ECommand {
+	constructor(client) {
+		super(client, {
+			aliases: ['disable'],
+			description: {
+				content:	'Disables a command in the server',
+				usage:		'<command name>',
+				examples:	['disable ping']
+			},
+			userPermissions: [Permissions.FLAGS.MANAGE_GUILD],
+			args: [
+				{
+					id:			'command',
+					type:		ArgConsts.WORD,
+					message:	'Please mention a command name'
+				}
+			],
+			guildOnly: false,
+			ownerOnly: false,
+		});
+	}
+
+	async run(message, {command}) {
+		const c = this.client.commandHandler.commands.get(command);
+
+		if (!c) {
+			throw `Command \`${command}\` not found`;
+		}
+
+		const entry = this.client.provider.get(message.guild, 'disabledCommands', {});
+
+		if (entry[command]) {
+			return `Command \`${command}\` is already disabled in this guild`;
+		}
+
+		entry[command] = true;
+
+		await this.client.provider.set(message.guild, 'disabledCommands', entry);
+
+		return `Disabled \`${command}\` in this server`;
+	}
+};
---
+++ commands/server/enable.js
@@ -0,0 +1,45 @@
+const {Permissions}			= require('discord.js');
+const {ArgConsts, ECommand}	= require('../../lib');
+
+module.exports = class extends ECommand {
+	constructor(client) {
+		super(client, {
+			aliases: ['enable'],
+			description: {
+				content:	'Enables a command in the server',
+				usage:		'<command name>',
+				examples:	['enable ping']
+			},
+			userPermissions: [Permissions.FLAGS.MANAGE_GUILD],
+			args: [
+				{
+					id:			'command',
+					type:		ArgConsts.WORD,
+					message:	'Please mention a command name'
+				}
+			],
+			guildOnly: false,
+			ownerOnly: false,
+		});
+	}
+
+	async run(message, {command}) {
+		const c = this.client.commandHandler.commands.get(command);
+
+		if (!c) {
+			throw `Command \`${command}\` not found`;
+		}
+
+		const entry = this.client.provider.get(message.guild, 'disabledCommands', {});
+
+		if (!entry[command]) {
+			return `Command \`${command}\` is already enabled in this guild`;
+		}
+
+		delete entry[command];
+
+		await this.client.provider.set(message.guild, 'disabledCommands', entry);
+
+		return `Enabled \`${command}\` in this server`;
+	}
+};
