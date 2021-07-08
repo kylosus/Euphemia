fb8ddb713d10faca96e778966d753a0a58ab3e82
jokersus <lolivampireslave@gmail.com>
2021-07-09 01:42:33 +0400

    Added a setup command (not implemented yet)

---
+++ commands/setup/setup.js
@@ -0,0 +1,27 @@
+const {Permissions}	= require('discord.js');
+const {ECommand}	= require('../../lib');
+
+module.exports = class extends ECommand {
+	constructor(client) {
+		super(client, {
+			actionName: 'setup',
+			aliases: ['setup'],
+			description: {
+				content:	'Sets up guild settings',
+				usage:		'',
+				examples:	['setup'],
+			},
+			userPermissions:	[Permissions.FLAGS.MANAGE_GUILD],
+			guildOnly: true,
+			ownerOnly: false,
+			disabled: true
+		});
+	}
+
+	// eslint-disable-next-line no-unused-vars
+	async run(message) {
+		// set up log channels
+		// set up muted role
+		return ':)';
+	}
+};
