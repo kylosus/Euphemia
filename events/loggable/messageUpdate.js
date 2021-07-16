398bf55003d72d5eff8bd4465a70918ae3ecbd19
jokersus <lolivampireslave@gmail.com>
2021-07-16 16:44:40 +0400

    Replaced manual truncating with lodash

--- events/loggable/messageUpdate.js
+++ events/loggable/messageUpdate.js
@@ -1,17 +1,14 @@
 const { MessageEmbed } = require('discord.js');
+const { EmbedLimits }  = require('../../lib');
+const _                = require('lodash');
 
 module.exports = async (channel, oldMessage, newMessage) => {
 	if (oldMessage.content === newMessage.content || !oldMessage.content || !newMessage.content) {
 		return;
 	}
 
-	if (oldMessage.content.length >= 1020) {
-		oldMessage.content = oldMessage.content.substring(0, 1020) + '...';
-	}
-
-	if (newMessage.content.length >= 1020) {
-		newMessage.content = newMessage.content.substring(0, 1020) + '...';
-	}
+	oldMessage.content = _.truncate(oldMessage.content, { length: EmbedLimits.FIELD_VALUE });
+	newMessage.content = _.truncate(newMessage.content, { length: EmbedLimits.FIELD_VALUE });
 
 	return channel.send(new MessageEmbed()
 		.setColor('PURPLE')
