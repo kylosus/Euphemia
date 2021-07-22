a944d11bc0ce327a73155967ca75865075c16043
jokersus <lolivampireslave@gmail.com>
2021-07-23 01:57:07 +0400

    Added inrole command

---
+++ commands/utility/inrole.js
@@ -0,0 +1,52 @@
+const { MessageEmbed }                      = require('discord.js');
+const { ArgConsts, ArgumentType, ECommand } = require('../../lib');
+const { CircularList, PaginatedMessage }    = require('../../modules');
+const _                                     = require('lodash');
+
+module.exports = class extends ECommand {
+	constructor(client) {
+		super(client, {
+			aliases:     ['inrole'],
+			description: {
+				content:  'Shows members in a given role',
+				usage:    '[user]',
+				examples: ['inrole 294846212895670273', 'inrole Something', 'avatar 275331662865367040']
+			},
+			args:        [
+				{
+					id:      'role',
+					type:    new ArgumentType(
+						/.*/,
+						ArgConsts.flatten,
+						({ guild }, roleRes) => {
+							return guild.roles.cache.get(roleRes) ||
+								guild.roles.cache.find(r => r.name.toLowerCase() === roleRes.toLowerCase()) ||
+								guild.roles.cache.find(r => r.name.toLowerCase().startsWith(roleRes.toLowerCase())) ||
+								(() => {
+									throw 'Role not found';
+								})();
+						}
+					),
+					message: 'Please provide a role',
+				},
+			],
+			guildOnly:   true,
+			ownerOnly:   false,
+			fetchMembers: true
+		});
+	}
+
+	async run(message, { role }) {
+		return role;
+	}
+
+	async ship(message, result) {
+		return PaginatedMessage.register(message, s => {
+			return new MessageEmbed()
+				.setColor(result.hexColor)
+				.setTitle(`List of members in ${result.name}`)
+				// .setDescription(s.map(ss => `${ss}, \`${ss.user.tag}\``).join('\n'));
+				.setDescription(s.map(s => s.user.tag));
+		}, new CircularList(_.chunk(result.members.array(), 20)));
+	}
+};
