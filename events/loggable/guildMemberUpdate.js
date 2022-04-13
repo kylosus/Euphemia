d983d83b681c3388709f8a0b64764320d0636b74
jokersus <lolivampireslave@gmail.com>
2022-04-13 15:12:26 +0300

    Updated for new d.js send()

--- events/loggable/guildMemberUpdate.js
+++ events/loggable/guildMemberUpdate.js
@@ -6,22 +6,24 @@ export default async (channel, oldMember, newMember) => {
 		const body = newMember.nickname ? `**${newMember.user.tag}** has changed their nickname` + (oldMember.nickname ? ` from **${oldMember.nickname}**` : '') + ` to **${newMember.nickname}**`
 			: `**${newMember.user.tag}** has removed their nickname, **${oldMember.nickname}**`;
 
-		return channel.send(new MessageEmbed()
-			.setColor('GREEN')
-			.setThumbnail(newMember.user.displayAvatarURL())
-			.setTitle('Nickname change')
-			.setDescription(body)
-			.setTimestamp()
-		);
+		return channel.send({
+			embeds: [new MessageEmbed()
+				.setColor('GREEN')
+				.setThumbnail(newMember.user.displayAvatarURL())
+				.setTitle('Nickname change')
+				.setDescription(body)
+				.setTimestamp()]
+		});
 	}
 
 	if (oldMember.user.tag !== newMember.user.tag) {
-		return channel.send(new MessageEmbed()
-			.setColor('GREEN')
-			.setThumbnail(newMember.user.displayAvatarURL())
-			.setTitle('Username change')
-			.setDescription(`**${oldMember.user.tag}** has changed their username to **${newMember.user.tag}**`)
-			.setTimestamp()
-		);
+		return channel.send({
+			embeds: [new MessageEmbed()
+				.setColor('GREEN')
+				.setThumbnail(newMember.user.displayAvatarURL())
+				.setTitle('Username change')
+				.setDescription(`**${oldMember.user.tag}** has changed their username to **${newMember.user.tag}**`)
+				.setTimestamp()]
+		});
 	}
 };
