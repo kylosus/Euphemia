8e1f34e88384b3d3c1e02a675e5dcb739f25a88d
jokersus <lolivampireslave@gmail.com>
2022-04-17 16:41:10 +0300

    Added intag command

---
+++ modules/subscription/commands/intag.js
@@ -0,0 +1,51 @@
+import { Formatters, MessageEmbed }       from 'discord.js';
+import { ArgConsts, ECommand }            from '../../../lib/index.js';
+import { getSubscribedUsers }             from '../db.js';
+import { CircularList, PaginatedMessage } from '../../paginatedmessage/index.js';
+import { chunk }                          from 'lodash-es';
+import { COLOR, PER_PAGE }                from './consts.js';
+
+
+export default class extends ECommand {
+	constructor(client) {
+		super(client, {
+			aliases:     ['intag'],
+			description: {
+				content:  'Shows all users in the tag without pinging',
+				usage:    '<name>',
+				examples: ['intag stuff']
+			},
+			args:        [
+				{
+					id:      'tagName',
+					type:    ArgConsts.TYPE.WORD,
+					message: 'Please enter a tag name'
+				}
+			],
+			guildOnly:   true,
+			ownerOnly:   false
+		});
+	}
+
+	async run(message, { tagName }) {
+		const res = await getSubscribedUsers({
+			guild: message.guild,
+			name:  tagName,
+		});
+
+		if (!res.length) {
+			throw `Tag ${Formatters.inlineCode(tagName)} not found or empty`;
+		}
+
+		return { tagName, users: res.map(r => r.user) };
+	}
+
+	async ship(message, { tagName, users }) {
+		return PaginatedMessage.register(message, s => {
+			return new MessageEmbed()
+				.setTitle(`User subscribed to ${tagName}`)
+				.setColor(COLOR)
+				.setDescription(s.map(Formatters.userMention).join('\n'));
+		}, new CircularList(chunk(users, PER_PAGE)));
+	}
+}
