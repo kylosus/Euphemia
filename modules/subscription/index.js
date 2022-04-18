2eaaf7aeab6d9b294ef05d490dca66844ccd5385
jokersus <lolivampireslave@gmail.com>
2022-04-18 22:23:35 +0300

    Removed db trace

--- modules/subscription/index.js
+++ modules/subscription/index.js
@@ -3,7 +3,5 @@ import { URL } from 'url';
 
 export const init = async (client, _db) => {
 	await db.init(client, _db);
-	_db.on('trace', console.log);
-
 	await client.commandHandler.registerCommands(new URL('./commands', import.meta.url).pathname, 'Subscription');
 };
