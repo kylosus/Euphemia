2569ba8b4946fd1e2a7e61b5e22277d9cc47e8d7
jokersus <lolivampireslave@gmail.com>
2022-04-23 00:41:41 +0300

    Temporarily disabling the cache module

--- modules/cache/index.js
+++ modules/cache/index.js
@@ -2,6 +2,7 @@ import * as db      from './db.js';
 import * as watcher from './watcher.js';
 
 export const init = async (client, _db) => {
-	await db.init(client, _db);
-	watcher.init(client, _db);
+	return;
+	// await db.init(client, _db);
+	// watcher.init(client, _db);
 };
