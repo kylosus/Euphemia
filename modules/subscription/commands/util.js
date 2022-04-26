68a6626a51c96ec02e42d5d0446ee32edea0dfa2
jokersus <lolivampireslave@gmail.com>
2022-04-26 23:31:44 +0300

    Forgot to stage util.js

--- modules/subscription/commands/util.js
+++ modules/subscription/commands/util.js
@@ -1,3 +1,16 @@
+import { Formatters }     from 'discord.js';
+import ArgumentType       from '../../../lib/Argument/ArgumentType.js';
+import { flatten }        from '../../../lib/Argument/ArgumentTypeConstants.js';
+import { MAX_TAG_LENGTH } from '../db.js';
+
+export const TagArgType = new ArgumentType(/^\w+[-_]?\w+$/, flatten, (_, s) => {
+	if (s.length > 25) {
+		throw `Tag name is too long. Max tag length is ${Formatters.inlineCode(String(MAX_TAG_LENGTH))}`;
+	}
+
+	return s;
+});
+
 export const chunk = (arr, maxLength) => {
 	const res = [];
 
