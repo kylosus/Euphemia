5f3debdaff181f9f908373cf8b59561a8aa9ca09
jokersus <lolivampireslave@gmail.com>
2022-04-26 23:20:39 +0300

    Message#member can be null

--- events/loggable/messageUpdate.js
+++ events/loggable/messageUpdate.js
@@ -1,6 +1,6 @@
 import { Formatters, MessageEmbed } from 'discord.js';
 import { EmbedLimits }              from '../../lib/index.js';
-import { truncate }     from 'lodash-es';
+import { truncate }                 from 'lodash-es';
 
 export default async (channel, oldMessage, newMessage) => {
 	if (oldMessage.content === newMessage.content || !oldMessage.content || !newMessage.content) {
@@ -14,7 +14,7 @@ export default async (channel, oldMessage, newMessage) => {
 		embeds: [new MessageEmbed()
 			.setColor('PURPLE')
 			.setTitle(`🖊 Message edited in #${newMessage.channel.name}`)
-			.setDescription(`${newMessage.member.toString() || 'Unknown user'} ${Formatters.inlineCode(newMessage.author?.id ?? 'Unknown id')} [Link](${newMessage.url})`)
+			.setDescription(`${newMessage.member?.toString() || 'Unknown user'} ${Formatters.inlineCode(newMessage.author?.id ?? 'Unknown id')} [Link](${newMessage.url})`)
 			.addField('Old message', oldMessage.content, false)
 			.addField('New message', newMessage.content, false)
 			.addField('ID', oldMessage.id, false)
