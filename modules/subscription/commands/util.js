58f148d343c867e8c9254a61e14f9a513d961e05
jokersus <lolivampireslave@gmail.com>
2022-04-26 23:34:47 +0300

    Actually using MAX_TAG_LENGTH to check

--- modules/subscription/commands/util.js
+++ modules/subscription/commands/util.js
@@ -4,7 +4,7 @@ import { flatten }        from '../../../lib/Argument/ArgumentTypeConstants.js';
 import { MAX_TAG_LENGTH } from '../db.js';
 
 export const TagArgType = new ArgumentType(/^\w+[-_]?\w+$/, flatten, (_, s) => {
-	if (s.length > 25) {
+	if (s.length > MAX_TAG_LENGTH) {
 		throw `Tag name is too long. Max tag length is ${Formatters.inlineCode(String(MAX_TAG_LENGTH))}`;
 	}
 
