c3c4241cfe00c6f621398f8679c8134e1080e9cf
jokersus <lolivampireslave@gmail.com>
2022-05-03 15:42:12 +0300

    Fixed wrong object key for expires

--- modules/mute/muteHandler.js
+++ modules/mute/muteHandler.js
@@ -1,7 +1,7 @@
 import { insert, remove } from './db.js';
 
 const muteMember = async (guild, member, mutedRole, reason, duration) => {
-	await insert({ guild, member, mutedRole, reason, duration });
+	await insert({ guild, member, mutedRole, reason, expires: duration });
 };
 
 // eslint-disable-next-line no-unused-vars
