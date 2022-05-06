092ee757708c408550b7a7253167979c2e82a1cf
jokersus <lolivampireslave@gmail.com>
2022-05-06 20:56:32 +0300

    Added invite link to dm notice

--- modules/autokick/watcher.js
+++ modules/autokick/watcher.js
@@ -1,16 +1,44 @@
 import { MessageEmbed } from 'discord.js';
 
+const ONE_DAY = 86400;	// seconds
+
 const onMemberAdd = client => {
 	client.on('guildMemberAdd', async member => {
 		if (!member.guild.autokick) {
 			return;
 		}
 
+		const inviteURL = await (async guild => {
+			if (guild.vanityURLCode) {
+				return `https://discord.gg/${guild.vanityURLCode}`;
+			}
+
+			try {
+				// Why is there a warning here?
+				const invite = await guild.invites.create(guild.channels.cache.first(), {
+					temporary: false,
+					maxAge:    ONE_DAY,
+					maxUses:   0,	// Unlimited
+					unique:    false,
+					reason:    'Autokick invite'
+				});
+
+				return invite.url;
+			} catch (err) {
+				return null;
+			}
+		})(member.guild);
+
 		await member.send({
 			embeds: [new MessageEmbed()
 				.setColor('DARK_RED')
 				.setTitle(`You have been kicked automatically from ${member.guild.name}`)
-				.setDescription('We are currently being raided. Please join back later. Sorry for the inconvenience caused.')
+				.setDescription(`
+					We are currently being raided.
+					Please join back later. Sorry for the inconvenience.
+				
+					${inviteURL ?? ''}
+				`)
 			]
 		});
 
