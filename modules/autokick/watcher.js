59766b3cfcc0bf38f5274e2617320b1a7c39fac5
jokersus <lolivampireslave@gmail.com>
2022-05-14 14:11:03 +0300

    Removed user notice from autokick to make it faster

--- modules/autokick/watcher.js
+++ modules/autokick/watcher.js
@@ -1,6 +1,6 @@
-import { MessageEmbed } from 'discord.js';
+// import { MessageEmbed } from 'discord.js';
 
-const ONE_DAY = 86400;	// seconds
+// const ONE_DAY = 86400;	// seconds
 
 const onMemberAdd = client => {
 	client.on('guildMemberAdd', async member => {
@@ -8,39 +8,39 @@ const onMemberAdd = client => {
 			return;
 		}
 
-		const inviteURL = await (async guild => {
-			if (guild.vanityURLCode) {
-				return `https://discord.gg/${guild.vanityURLCode}`;
-			}
-
-			try {
-				// Why is there a warning here?
-				const invite = await guild.invites.create(guild.channels.cache.first(), {
-					temporary: false,
-					maxAge:    ONE_DAY,
-					maxUses:   0,	// Unlimited
-					unique:    false,
-					reason:    'Autokick invite'
-				});
-
-				return invite.url;
-			} catch (err) {
-				return null;
-			}
-		})(member.guild);
-
-		await member.send({
-			embeds: [new MessageEmbed()
-				.setColor('DARK_RED')
-				.setTitle(`You have been kicked automatically from ${member.guild.name}`)
-				.setDescription(`
-					We are currently being raided.
-					Please join back later. Sorry for the inconvenience.
-				
-					${inviteURL ?? ''}
-				`)
-			]
-		});
+		// const inviteURL = await (async guild => {
+		// 	if (guild.vanityURLCode) {
+		// 		return `https://discord.gg/${guild.vanityURLCode}`;
+		// 	}
+		//
+		// 	try {
+		// 		// Why is there a warning here?
+		// 		const invite = await guild.invites.create(guild.channels.cache.first(), {
+		// 			temporary: false,
+		// 			maxAge:    ONE_DAY,
+		// 			maxUses:   0,	// Unlimited
+		// 			unique:    false,
+		// 			reason:    'Autokick invite'
+		// 		});
+		//
+		// 		return invite.url;
+		// 	} catch (err) {
+		// 		return null;
+		// 	}
+		// })(member.guild);
+		//
+		// await member.send({
+		// 	embeds: [new MessageEmbed()
+		// 		.setColor('DARK_RED')
+		// 		.setTitle(`You have been kicked automatically from ${member.guild.name}`)
+		// 		.setDescription(`
+		// 			We are currently being raided.
+		// 			Please join back later. Sorry for the inconvenience.
+		//
+		// 			${inviteURL ?? ''}
+		// 		`)
+		// 	]
+		// });
 
 		return await member.kick();
 	});
