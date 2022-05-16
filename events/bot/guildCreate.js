53274723217975fbbee84c3d9abfe6dc3c596aa9
jokersus <lolivampireslave@gmail.com>
2022-05-16 16:44:47 +0300

    Fixed embed (string fields, typo, etc.)

--- events/bot/guildCreate.js
+++ events/bot/guildCreate.js
@@ -1,4 +1,4 @@
-import { MessageEmbed } from 'discord.js';
+import { Formatters, MessageEmbed } from 'discord.js';
 
 const DISCORD_LOGO = 'https://cdn.discordapp.com/attachments/540834912366755850/547465681453449246/discord-logo.png';
 
@@ -9,12 +9,12 @@ export default async guild => {
 		return owner.send({
 			embeds: [new MessageEmbed()
 				.setColor(guild.client.defaultColor)
-				.setImage(guild.iconURL() || DISCORD_LOGO)
+				.setImage(guild.iconURL() ?? DISCORD_LOGO)
 				.setTitle('Bot has joined a new guild')
-				.addField('Guild name', guild.name)
-				.addField('Guild id', guild.id || '-')
-				.addField('Member count', guild.memberCount || '-')
-				.addField('Owner', guild.owner ? guild.owner.user.tag : guild.ownerID)]
+				.addField('Guild name', guild.name, true)
+				.addField('Guild id', guild.id || '-', true)
+				.addField('Member count', String(guild.memberCount) || '-', true)
+				.addField('Owner', Formatters.userMention(guild.ownerId), true)]
 		});
 	});
 };
