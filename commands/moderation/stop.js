b2ec2e2fb94df3fe98632a6eecbdcb8435b8e5be
jokersus <lolivampireslave@gmail.com>
2022-05-18 14:35:29 +0300

    Replaced old d.js permission overwrite stuff

--- commands/moderation/stop.js
+++ commands/moderation/stop.js
@@ -45,7 +45,7 @@ export default class extends ModerationCommand {
 
 		await Promise.all(channels.map(async c => {
 			try {
-				await c.updateOverwrite(message.guild.id, { SEND_MESSAGES: toggle });
+				await c.permissionOverwrites.edit(message.guild.id, { SEND_MESSAGES: toggle });
 			} catch (err) {
 				return result.addFailed(c, err.message || 'Unknown error');
 			}
