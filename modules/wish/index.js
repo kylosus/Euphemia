dc21942fa198299c3f860b5ec4a13b94d488049a
jokersus <lolivampireslave@gmail.com>
2022-07-01 23:32:41 +0300

    Added wish module and overwrite for messageDeleted event

---
+++ modules/wish/index.js
@@ -0,0 +1,7 @@
+import * as db from './db.js';
+import { URL } from 'url';
+
+export const init = async (client, _db) => {
+	await db.init(client, _db);
+	await client.commandHandler.registerCommands(new URL('./commands', import.meta.url).pathname, 'Wish');
+};
