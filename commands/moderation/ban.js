53c259b7a1f44d62b8b75f88ccfcc8592b09734d
jokersus <lolivampireslave@gmail.com>
2022-09-11 00:34:05 +0400

    Added tragiaclly obliterated embed

--- commands/moderation/ban.js
+++ commands/moderation/ban.js
@@ -1,4 +1,4 @@
-import { Permissions }                                from 'discord.js';
+import { Permissions, MessageEmbed }                  from 'discord.js';
 import { ArgConsts }                                  from '../../lib/index.js';
 import { ModerationCommand, ModerationCommandResult } from '../../modules/moderation/index.js';
 
@@ -52,5 +52,27 @@ export default class extends ModerationCommand {
 		}));
 
 		return result;
+	}
+
+	async ship(message, result) {
+		const embed = new MessageEmbed()
+			.setColor(result.getColor())
+			.setTitle('Users have been tragically obliterated')
+			.setImage('https://cdn.discordapp.com/attachments/420272882900533248/1018256667122597888/unknown.png');
+
+		if (result.aux) {
+			embed.setDescription(result.aux.toString());
+		}
+
+		embed.addField('Casualties', result.passed.map(r => r.toString()).join(' ') || '~');
+
+		if (result.failed.length) {
+			embed.addField('Survivors', result.failed.map(r => `${r.toString()} - ${r.reason || '**Unknown reason**'}`).join(' '));
+		}
+
+		embed.addField('Moderator', message.member.toString(), true);
+		embed.addField('Reason', result?.reason ?? '*No reason provided*', true);
+
+		return message.channel.send({ embeds: [embed] });
 	}
 }
