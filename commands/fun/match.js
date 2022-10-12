0d01592a3be77b928a92bdbcbbd69c5de6e48da1
jokersus <lolivampireslave@gmail.com>
2022-10-12 22:17:16 +0300

    Fixed typo

--- commands/fun/match.js
+++ commands/fun/match.js
@@ -38,7 +38,7 @@ export default class extends ECommand {
 	async run(message, { user }) {
 		// hehe
 		if (message.author.id === ME && user.toLowerCase() === 'pablo escobar') {
-			return { user, match: 10000000000000 });
+			return { user, match: 10000000000000 };
 		}
 
 		const error = new EmbedError('Please mention someone to match with');
