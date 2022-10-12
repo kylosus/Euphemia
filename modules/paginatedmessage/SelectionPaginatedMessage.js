363618fb52df8252e348c662c58770acbd8af4c2
jokersus <lolivampireslave@gmail.com>
2022-10-12 21:26:05 +0300

    Added special handling for interaction messages

--- modules/paginatedmessage/SelectionPaginatedMessage.js
+++ modules/paginatedmessage/SelectionPaginatedMessage.js
@@ -17,6 +17,23 @@ const _watcher = ({ message, generator, options }) => {
 	});
 };
 
+const _watcher_interaction = ({ originalInteraction, interactionReply, generator, options }) => {
+	// handle errors!
+	return interactionReply.createMessageComponentCollector({
+		filter: async interaction => {
+			if (!interaction.isSelectMenu()) {
+				return;
+			}
+
+			// Possibly unsafe
+			const embed = generator(options[interaction.values[0]].data);
+			originalInteraction.editReply({ embeds: [embed] });
+
+			await interaction.deferUpdate();
+		}
+	});
+};
+
 const register = async (message, generator, options) => {
 	const current = options[0].data;
 
@@ -36,7 +53,17 @@ const register = async (message, generator, options) => {
 
 	const botMessage = await message.reply({ embeds: [firstEmbed], components: [row] });
 
-	_watcher({ message: botMessage, generator, options });
+	// ?????
+	if (message.isChatInputCommand()) {
+		return _watcher_interaction({
+			originalInteraction: message,
+			interactionReply: botMessage,
+			generator,
+			options
+		});
+	}
+
+	return _watcher({ message: botMessage, generator, options });
 };
 
 export {
