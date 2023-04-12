e8ca325c95506ee8e5acce0789354f173c769592
jokersus <lolivampireslave@gmail.com>
2023-04-12 18:22:45 +0300

    Added automatic music party stuff

--- modules/vc/watcher.js
+++ modules/vc/watcher.js
@@ -1,55 +1,78 @@
-const BOT_ID     = '425520602208796673';
-const GUILD_ID   = '292277485310312448';
-const CHANNEL_ID = '293343835122171924';
+const BOT_ID                          = '425520602208796673';
+const VOICE_CHANNEL_ID                = '293343835122171924';
+const MUSIC_CHANNEL_ID                = '293342449131061248';
+const PARTY_CATEGORY_ID               = '1095725848054734919';
+const REGULAR_CATEGORY_ID             = '372325226010116096';
+const VOICE_CHANNEL_NAME              = 'Music 🎵';
+const MUSIC_CHANNEL_REGULAR_POSITION  = 2;
+const PARTY_CATEGORY_PARTY_POSITION   = 2;
+const PARTY_CATEGORY_REGULAR_POSITION = 10;
 
-const INTERVAL_MINUTES = 15;
+// Init can handle all this
+
+let INTERVAL_ID = null;
+
+const INTERVAL_MINUTES = 5;
 const MILLISECOND      = 60 * 1000;
 
-// const ready = client => {
-// 	const guild   = client.guilds.cache.get(GUILD_ID);
-// 	const channel = guild.channels.cache.get(CHANNEL_ID);
-//
-// 	const channelNameBase = channel.name;
-//
-// 	let currentHours = 0;
-//
-// 	let intervalID = null;
-//
-// 	intervalID = setInterval((function setName() {
-// 		// Check if the bot is the only member
-// 		if (channel.members.size < 2) {
-// 			if (!intervalID) {
-// 				return setName;
-// 			}
-//
-// 			if (channel.members.has(BOT_ID)) {
-// 				clearInterval(intervalID);
-// 				return;
-// 			}
-// 		}
-//
-// 		channel.setName(`[${currentHours} hours] ${channelNameBase}`).catch(console.error);
-// 		currentHours += INTERVAL_MINUTES / 60;
-//
-// 		return setName;
-// 	})(), INTERVAL_MINUTES * MILLISECOND);
-// };
-
-const voiceStateUpdate = (oldState, newState) => {
+const voiceStateUpdate = async (oldState, newState) => {
 	// Ignore bot
 	if (newState.id === BOT_ID) {
 		return;
 	}
 
 	// Newly joined channel
-	if (newState.channel?.id === CHANNEL_ID && !oldState.channel) {
-		return;
+	if (newState.channel?.id === VOICE_CHANNEL_ID && !oldState.channel) {
+		const voiceChannel = newState.channel;
+
+		// Not enough members
+		if (voiceChannel.members.size < 2) {
+			return;
+		}
+
+		// Enough members. Move up
+		const partyCategory = voiceChannel.guild.channels.cache.get(PARTY_CATEGORY_ID);
+		const musicChannel  = voiceChannel.guild.channels.cache.get(MUSIC_CHANNEL_ID);
+
+		partyCategory.setPosition(PARTY_CATEGORY_PARTY_POSITION);
+
+		await voiceChannel.setParent(partyCategory);
+		await musicChannel.setParent(partyCategory);
+
+		// Start the count
+		const channelNameBase = voiceChannel.name;
+
+		let currentHours = 0;
+
+		INTERVAL_ID = setInterval(() => {
+			voiceChannel.setName(`[${currentHours} hours] ${VOICE_CHANNEL_NAME}`).catch(console.error);
+			currentHours += INTERVAL_MINUTES / 60;
+		}, INTERVAL_MINUTES * MILLISECOND);
 	}
 
-
 	// Left the channel
-	if (oldState.channel?.id === CHANNEL_ID && !newState.channel) {
+	if (oldState.channel?.id === VOICE_CHANNEL_ID && !newState.channel) {
+		const voiceChannel = oldState.channel;
 
+		// Enough members
+		if (voiceChannel.members.size > 2) {
+			return;
+		}
+
+		// Not enough members. Move down
+		const regularCategory = voiceChannel.guild.channels.cache.get(REGULAR_CATEGORY_ID);
+		const partyCategory   = voiceChannel.guild.channels.cache.get(PARTY_CATEGORY_ID);
+		const musicChannel    = voiceChannel.guild.channels.cache.get(MUSIC_CHANNEL_ID);
+
+		await voiceChannel.setParent(regularCategory);
+		await musicChannel.setParent(regularCategory);
+
+		await musicChannel.setPosition(MUSIC_CHANNEL_REGULAR_POSITION);
+		await partyCategory.setPosition(PARTY_CATEGORY_REGULAR_POSITION);
+
+		// Stop the count
+		clearInterval(INTERVAL_ID);
+		await voiceChannel.setName(VOICE_CHANNEL_NAME);
 	}
 };
 
